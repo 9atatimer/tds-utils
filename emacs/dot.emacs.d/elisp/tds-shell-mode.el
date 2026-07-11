3a469f4f72d6d363d6958413e212048cbf11086c
Todd Stumpf <tds@nineatatime.net>
2026-07-11 13:10:04 -1000

    fix(emacs): untrack stale files, scrub legacy hostnames, and soften phrasing

--- emacs/dot.emacs.d/elisp/tds-shell-mode.el
+++ emacs/dot.emacs.d/elisp/tds-shell-mode.el
@@ -2,7 +2,7 @@
 ;; Shell Edit Mode
 
 (defun tds-setup-shell-mode ()
-  "Customize the crap out of shell-mode (aka sh-mode).
+  "Customize shell-mode (aka sh-mode).
 
 Can be called directly, or added to the sh-mode-hook, as you will.
 "
